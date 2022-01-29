f69a55bd482a0d0f9e15896abf4df68926682306
nathan <86123602+noxJ@users.noreply.github.com>
2022-01-29 15:32:36 +0000

    Added basic starter code

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(1280, 720)
+	ebiten.SetWindowTitle("Sunrise")
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Panic("Failed to run game: ", err)
+	}
+}
