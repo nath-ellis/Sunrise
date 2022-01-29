56e1ca27e53cb8d71e67d0f49bffa2d1e5bb2ac2
nathan <86123602+noxJ@users.noreply.github.com>
2022-01-29 17:43:55 +0000

    Added a player to the screen

--- main.go
+++ main.go
@@ -6,17 +6,32 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/solarlune/resolv"
 )
 
+type Player struct {
+	Obj  *resolv.Object
+	Icon *ebiten.Image
+}
+
 var (
-	State string = "menu"
-	BG    *ebiten.Image
+	State  string = "menu"
+	BG     *ebiten.Image
+	player Player
+	Space  *resolv.Space
 )
 
 type Game struct{}
 
 func init() {
 	BG, _, _ = ebitenutil.NewImageFromFile("assets/bg.png")
+
+	Space = resolv.NewSpace(1280, 720, 16, 16)
+
+	player.Icon, _, _ = ebitenutil.NewImageFromFile("assets/character1.png")
+	player.Obj = resolv.NewObject(622/4, 300/4, 36, 56, "player") // divided by 4 as the image is scaled up by 4
+
+	Space.Add(player.Obj)
 }
 
 func (g *Game) Update() error {
@@ -38,6 +53,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	switch State {
 	case "menu":
 	case "game":
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(player.Obj.X, player.Obj.Y)
+		op.GeoM.Scale(4, 4)
+		screen.DrawImage(player.Icon, op)
 	case "gameOver":
 	}
 }
