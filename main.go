a031f8f6b96c69ace2623efd3b5441795b16cc37
nathan <86123602+noxJ@users.noreply.github.com>
2022-01-29 16:52:56 +0000

    Added a background

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var (
+	BG *ebiten.Image
 )
 
 type Game struct{}
+
+func init() {
+	BG, _, _ = ebitenutil.NewImageFromFile("assets/bg.png")
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-
+	screen.DrawImage(BG, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
