c5116d8122380500824510a2e08a2feadb371c35
nathan <86123602+noxJ@users.noreply.github.com>
2022-01-29 18:44:59 +0000

    Added a function for creating objects in the scenery

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "image/png"
 	"log"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -14,11 +15,17 @@ type Player struct {
 	Icon *ebiten.Image
 }
 
+type Object struct {
+	Obj  *resolv.Object
+	Type string
+}
+
 var (
-	State  string = "menu"
-	BG     *ebiten.Image
-	player Player
-	Space  *resolv.Space
+	State   string = "menu"
+	BG      *ebiten.Image
+	player  Player
+	Space   *resolv.Space
+	Objects []Object
 )
 
 type Game struct{}
@@ -32,6 +39,59 @@ func init() {
 	player.Obj = resolv.NewObject(622/4, 300/4, 36, 56, "player") // divided by 4 as the image is scaled up by 4
 
 	Space.Add(player.Obj)
+}
+
+func newObject() {
+	yUp := player.Obj.Y - (1280 - 720)
+	yDown := player.Obj.Y + (1280 + 720)
+	xLeft := player.Obj.X - (1280 * 2)
+	xRight := player.Obj.X + (1280 * 2)
+
+	randUp := rand.Intn(10)
+	randDown := rand.Intn(10)
+	randLeft := rand.Intn(10)
+	randRight := rand.Intn(10)
+
+	for i := 0; i > randUp; i++ {
+		x := rand.Intn(1280)
+		y := rand.Intn(int(yUp))
+
+		if y > 0 {
+			y -= 720
+		}
+
+		Objects = append(Objects, Object{resolv.NewObject(float64(x), float64(y), 32, 32, "object"), "tree"})
+	}
+	for i := 0; i > randDown; i++ {
+		x := rand.Intn(1280)
+		y := rand.Intn(int(yDown))
+
+		if y < 720 {
+			y += 720
+		}
+
+		Objects = append(Objects, Object{resolv.NewObject(float64(x), float64(y), 32, 32, "object"), "tree"})
+	}
+	for i := 0; i > randLeft; i++ {
+		x := rand.Intn(int(xLeft))
+		y := rand.Intn(720)
+
+		if y > 0 {
+			y -= 1280
+		}
+
+		Objects = append(Objects, Object{resolv.NewObject(float64(x), float64(y), 32, 32, "object"), "tree"})
+	}
+	for i := 0; i > randRight; i++ {
+		x := rand.Intn(int(xRight))
+		y := rand.Intn(720)
+
+		if y < 1280 {
+			y += 1280
+		}
+
+		Objects = append(Objects, Object{resolv.NewObject(float64(x), float64(y), 32, 32, "object"), "tree"})
+	}
 }
 
 func (g *Game) Update() error {
